f3b00d8382d1d329e3581232cfaa05caf7b4a2ae
Niggneel <125867328+Niggneel@users.noreply.github.com>
2024-01-02 22:33:56 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ import axios from 'axios'
 global.owner = [
   ['917605902011', 'GURU', true],
   ['6281255369012', 'Ash', true], 
-  [''] 
+  ['91626609278'] , 'meet', true ],
 ] //Number of owners
 
 //global.pairingNumber = "" //put your bot number here
@@ -50,7 +50,7 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'ᴛʜᴇ ɢᴜʀᴜ-ʙᴏᴛ'
+global.botname = 'ᴛʜᴇ akameet-bot'
 global.premium = 'true'
 global.packname = 'GURU┃ᴮᴼᵀ' 
 global.author = '@Asliguru'
